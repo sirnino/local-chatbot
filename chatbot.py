5ef01d752fd259b1ed94fc1845de1d65a320173f
Antonino Sirchia <antonino.sirchia@elca.ch>
2025-03-06 15:48:11 +0100

    Add streaming and chat history

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,17 @@
 from langchain_ollama.chat_models import ChatOllama
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 
-OLLAMA_BASEURL='http://192.168.1.99:11434/'
-
-def get_chatbot_chain(model='llama3.2:latest', temperature=0.3):
+def get_chatbot_chain(model='llama3.2:latest', base_url='http://127.0.0.1:11434', temperature=0.3):
     llm = ChatOllama(
             model = model,
             temperature = temperature,
-            base_url = OLLAMA_BASEURL
+            base_url = base_url
         )
 
     prompt = ChatPromptTemplate(messages=[
         ("system", "You are a helpful AI bot. Your name is Lumina."),
+        MessagesPlaceholder("history"),
         ("human", "{user_input}"),
     ])
 
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import streamlit as st
 from chatbot import get_chatbot_chain
 from ollama import Client
 
+OLLAMA_BASEURL='http://192.168.1.99:11434'
+
 client = Client(
-  host='http://192.168.1.99:11434'
+  host=OLLAMA_BASEURL
 )
 ollama_models = [m.model for m in client.list()['models']]
 
@@ -12,20 +14,21 @@ with st.sidebar:
     model = st.selectbox("Model", ollama_models)
     temperature = st.slider("Temperature", 0.0, 1.0, 0.5)
 
-chain = get_chatbot_chain("llama3.2:latest", temperature)
+chain = get_chatbot_chain("llama3.2:latest", base_url=OLLAMA_BASEURL, temperature=temperature)
 st.title("Chat with Lumina")
-st.write(f"Using model: {model}")
+st.caption(f"Using model: {model}")
 
 messages = st.container()
 # Session State initialization
 if 'messages' not in st.session_state:
     st.session_state['messages'] = []
 
-if user_input := st.chat_input("Say something"):
-    st.session_state['messages'].append(("user", user_input))
-    response = chain.invoke({"user_input": user_input})
-    st.session_state['messages'].append(("assistant", response))
-
 for msg in st.session_state['messages']:
     with messages:
         st.chat_message(msg[0]).write(msg[1])
+
+if user_input := st.chat_input("Say something"):
+    st.chat_message("user").write(user_input)
+    response = st.chat_message("assistant").write_stream(chain.stream({"history":  st.session_state['messages'], "user_input": user_input}))
+    st.session_state['messages'].append(("user", user_input))
+    st.session_state['messages'].append(("assistant", response))
